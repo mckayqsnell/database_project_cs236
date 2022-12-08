921ee782b9f10dd4bb4b07a96dfc24e706a4ca87
McKay Snell <mckayqsnell@gmail.com>
2022-12-07 22:31:14 -0700

    linux warnings

--- Graph.cpp
+++ Graph.cpp
@@ -2,7 +2,7 @@
 
 Graph::Graph(unsigned int size)
 {
-    for(int i = 0; i < size; i++)
+    for(unsigned int i = 0; i < size; i++)
     {
         graph[i] = set<int>();
         reverseGraph[i] = set<int>(); // make sure this is working.
